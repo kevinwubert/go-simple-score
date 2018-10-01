6f260dbfd46297a9e3bfe30c452a78b7dede5024
Kevin Wu <kevinwubert@gmail.com>
2018-09-30 20:53:19 -0400

    writing response in handlers

--- pkg/score/score.go
+++ pkg/score/score.go
@@ -1,6 +1,9 @@
 package score
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+)
 
 type client struct {
 	scoreValue int
@@ -22,22 +25,43 @@ func (c *client) Get() int {
 	return c.scoreValue
 }
 
-func (c *client) Set(val int) {
+func (c *client) Set(val int) int {
 	c.scoreValue = val
+	return c.scoreValue
 }
 
-func (c *client) Add(val int) {
+func (c *client) Add(val int) int {
 	c.scoreValue += val
+	return c.scoreValue
 }
 
 func (c *client) GetHandler(w http.ResponseWriter, r *http.Request) {
+	s := c.Get()
 
+	WriteJSONResponse(w, map[string]int{
+		"score": s,
+	})
 }
 
 func (c *client) SetHandler(w http.ResponseWriter, r *http.Request) {
+	s := c.Set(val)
 
+	WriteJSONResponse(w, map[string]int{
+		"score": s,
+	})
 }
 
 func (c *client) AddHandler(w http.ResponseWriter, r *http.Request) {
+	s := c.Add(val)
 
+	WriteJSONResponse(w, map[string]int{
+		"score": s,
+	})
+}
+
+// WriteJSONResponse writes some value and encodes into the response
+func WriteJSONResponse(w http.ResponseWriter, val interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(val)
 }
