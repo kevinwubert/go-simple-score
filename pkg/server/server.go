7bba619ec007cc1973a983ab845751d3f16f8f58
Kevin Wu <kevinwubert@gmail.com>
2018-12-03 21:03:28 -0800

    adding transform for vector

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -4,6 +4,8 @@ import (
 	"net/http"
 
 	"github.com/kevinwubert/go-simple-score/pkg/score"
+	"github.com/kevinwubert/go-simple-score/pkg/transform"
+
 	log "github.com/sirupsen/logrus"
 )
 
@@ -13,9 +15,17 @@ func Main() error {
 	initScore := 0
 	scoreClient := score.New(initScore)
 
+	initPos := transform.NewVector(0, 0, 0)
+	initRot := transform.NewVector(0, 0, 0)
+
+	transformClient := transform.New(initPos, initRot)
+
 	http.HandleFunc("/getScore", scoreClient.GetHandler)
 	http.HandleFunc("/setScore", scoreClient.SetHandler)
 	http.HandleFunc("/addScore", scoreClient.AddHandler)
+
+	http.HandleFunc("/getTransform", transformClient.GetHandler)
+	http.HandleFunc("/setTransform", transformClient.SetHandler)
 
 	err := http.ListenAndServe(":62802", nil)
 	return err
---
+++ pkg/transform/transform.go
@@ -0,0 +1,93 @@
+package transform
+
+import (
+	"encoding/json"
+	"net/http"
+
+	log "github.com/sirupsen/logrus"
+)
+
+type Vector struct {
+	x float32
+	y float32
+	z float32
+}
+
+func NewVector(x float32, y float32, z float32) Vector {
+	return Vector{x: 0, y: 0, z: 0}
+}
+
+type client struct {
+	pos Vector
+	rot Vector
+}
+
+type Client interface {
+	GetHandler(w http.ResponseWriter, r *http.Request)
+	SetHandler(w http.ResponseWriter, r *http.Request)
+}
+
+func New(defaultPos Vector, defaultRot Vector) Client {
+	return &client{
+		pos: defaultPos,
+		rot: defaultRot,
+	}
+}
+
+func (c *client) Get() (Vector, Vector) {
+	return c.pos, c.rot
+}
+
+func (c *client) Set(pos Vector, rot Vector) (Vector, Vector) {
+	c.pos = pos
+	c.rot = rot
+
+	return c.pos, c.rot
+}
+
+func (c *client) GetHandler(w http.ResponseWriter, r *http.Request) {
+	pos, rot := c.Get()
+
+	WriteJSONResponse(w, map[string]Vector{
+		"pos": pos,
+		"rot": rot,
+	})
+}
+
+type requestFormat struct {
+	Pos Vector `json:"pos"`
+	Rot Vector `json:"rot"`
+}
+
+func (c *client) SetHandler(w http.ResponseWriter, r *http.Request) {
+	decoder := json.NewDecoder(r.Body)
+	var rf requestFormat
+	err := decoder.Decode(&rf)
+
+	if err != nil {
+		log.Errorf("decoder failed to decode: %v", err)
+		WriteErrorResponse(w, err)
+		return
+	}
+
+	po := rf.Pos
+	ro := rf.Rot
+	pos, rot := c.Set(po, ro)
+
+	WriteJSONResponse(w, map[string]Vector{
+		"pos": pos,
+		"rot": rot,
+	})
+}
+
+// WriteErrorResponse writes an error back from an invalid request
+func WriteErrorResponse(w http.ResponseWriter, err error) {
+	http.Error(w, err.Error(), http.StatusBadRequest)
+}
+
+// WriteJSONResponse writes some value and encodes into the response
+func WriteJSONResponse(w http.ResponseWriter, val interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(val)
+}
