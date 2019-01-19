297d9da20f4fae8218279cd3428c6007df8f0c6b
Kevin Wu <kevinwubert@gmail.com>
2019-01-19 11:30:51 -0800

    adding another transform

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -20,12 +20,19 @@ func Main() error {
 
 	transformClient := transform.New(initPos, initRot)
 
+	initPos2 := transform.NewVector(0, 0, 0)
+	initRot2 := transform.NewVector(0, 0, 0)
+
+	transformClient2 := transform.New(initPos2, initRot2)
+
 	http.HandleFunc("/getScore", scoreClient.GetHandler)
 	http.HandleFunc("/setScore", scoreClient.SetHandler)
 	http.HandleFunc("/addScore", scoreClient.AddHandler)
 
 	http.HandleFunc("/getTransform", transformClient.GetHandler)
 	http.HandleFunc("/setTransform", transformClient.SetHandler)
+	http.HandleFunc("/getTransform2", transformClient2.GetHandler)
+	http.HandleFunc("/setTransform2", transformClient2.SetHandler)
 
 	err := http.ListenAndServe(":80", nil)
 	return err
